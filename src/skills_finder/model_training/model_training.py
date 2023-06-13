e0029835986760db39b55b306c8d180a1e9deaa4
Sans1509 <sanskriti.tiwari@knoldus.com>
2023-06-13 19:03:40 +0530

    new changes

--- src/skills_finder/model_training/model_training.py
+++ src/skills_finder/model_training/model_training.py
@@ -1,6 +1,7 @@
 from gensim.models import Word2Vec
 import pandas as pd
-
+import nltk
+from nltk.tokenize import sent_tokenize
 from src.utils.constants import file_path
 
 
@@ -13,7 +14,7 @@ def model_training():
     sentences = data['skill_set'].tolist()
 
     tokenized_sentences = [sentence.split() for sentence in sentences]
-
+    print(tokenized_sentences)
     model = Word2Vec(tokenized_sentences, vector_size=100, window=5, min_count=1, workers=4)
     word_vectors = model.wv
     return word_vectors
--- src/skills_finder/model_validation/model_validation.py
+++ src/skills_finder/model_validation/model_validation.py
@@ -3,27 +3,11 @@ def model_validation(trained_model, extracted_text):
     getting the word vector and checking for similarity and returning the word
     who have similarity of one
     @return extracted skills
-    @param extracted_text:
-    @param trained_model:
+    @param extracted_text
+    @param trained_model
     """
     word_vectors = trained_model
     extracted_data = extracted_text
 
     valid_tokens = [word for word in extracted_data if word in word_vectors.key_to_index]
-    print(valid_tokens)
-
-    "test_tokens are the test_tokens = test_sentence.split() , test_sentence is the input text"
-    if valid_tokens:
-        avg_vector = sum(word_vectors.get_vector(word) for word in valid_tokens) / len(valid_tokens)
-        similar_words = word_vectors.similar_by_vector(avg_vector)
-
-        extracted_skills = []
-
-        for word, similarity in similar_words:
-            if similarity == 0.1:
-                extracted_skills.append(word)
-
-        return extracted_skills
-
-    else:
-        return "No valid tokens found in the vocabulary."
+    return valid_tokens
